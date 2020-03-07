1872c2c96e4038ab9af1e127e1acdcfb10783cae
kseyko35 <seyfiercan35@hotmail.com>
2020-03-07 12:48:38 +0300

    we can pull user's data  from facebook

--- app/src/main/java/com/example/facebooklogin/MainActivity.kt
+++ app/src/main/java/com/example/facebooklogin/MainActivity.kt
@@ -1,42 +1,70 @@
 package com.example.facebooklogin
 
+/**     Code with ❤
+╔════════════════════════════╗
+║   Created by Seyfi ERCAN   ║
+╠════════════════════════════╣
+║  seyfiercan35@hotmail.com  ║
+╠════════════════════════════╣
+║      07,March,2020         ║
+╚════════════════════════════╝
+ */
+
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
-import com.facebook.login.LoginManager
+import com.bumptech.glide.Glide
+import com.facebook.*
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
-import java.util.*
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity(), FacebookCallback<LoginResult> {
     private var callbackManager: CallbackManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        callbackManager = CallbackManager.Factory.create()
+        val btnFacebookLogin: LoginButton = findViewById(R.id.login_button)
 
-        LoginManager.getInstance().logInWithReadPermissions(
-            this,
-            Arrays.asList("public_profile")
-        )
-        // If you are using in a fragment, call loginButton.setFragment(this);
-        // If you are using in a fragment, call loginButton.setFragment(this);
-        val btnFacebookLogin: LoginButton = this.findViewById(R.id.login_button)
-        val callbackManager = CallbackManager.Factory.create()
+
+        callbackManager = CallbackManager.Factory.create()
         btnFacebookLogin.registerCallback(callbackManager, this)
-//        loginButton = findViewById(R.id.login_button);
-//        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+
+
+        var tokenTracker: AccessTokenTracker = object : AccessTokenTracker() {
+            override fun onCurrentAccessTokenChanged(
+                oldAccessToken: AccessToken?,
+                currentAccessToken: AccessToken?
+            ) {
+                if (currentAccessToken == null) {
+                    name.text = ""
+                    profile_image.setImageResource(R.mipmap.ic_launcher)
+                    Toast.makeText(this@MainActivity, "User Loged out", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
     }
 
+
     override fun onSuccess(result: LoginResult?) {
-        print(result)
+
+        val request = GraphRequest.newMeRequest(result?.accessToken) { `object`, response ->
+            displayUserInfo(`object`)
+
+        }
+
+        val parameters = Bundle()
+        parameters.putString("fields", "name,id")
+        request.parameters = parameters
+        request.executeAsync()
         Toast.makeText(this@MainActivity, "onSuccess", Toast.LENGTH_SHORT).show()
     }
 
@@ -56,5 +84,20 @@ class MainActivity : AppCompatActivity(), FacebookCallback<LoginResult> {
         callbackManager?.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
     }
+
+    private fun displayUserInfo(jsonObject: JSONObject?) {
+        if (jsonObject != null) {
+            val circleImageView: CircleImageView = findViewById(R.id.profile_image)
+            val nameTxtView: TextView = findViewById(R.id.name)
+
+            Glide.with(this)
+                .load("http://graph.facebook.com/" + jsonObject.getString("id") + "/picture?type=large")
+                .into(circleImageView)
+            nameTxtView.text = jsonObject.getString("name")
+
+        }
+    }
+
+
 }
 
